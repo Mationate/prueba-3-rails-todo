01d31661ce8af20ea09961e5ccaf7b97d9be68f7
Matias Oñate <mationate.0@gmail.com>
2019-01-30 11:19:53 -0300

    show method working

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,9 +1,41 @@
 class TodosController < ApplicationController
+  before_action :set_todo, only: %i[show]
 
   def index
     @todos = Todo.all
   end
 
+  def new
+    @todo = Todo.new
+  end
+
+  def create
+    todo = Todo.new(todo_params)
+
+    if todo.save
+      redirect_to root_path, notice: 'Se guardó la tarea con éxito'
+    else
+      redirect_to root_path, alert: 'No se guardó la tarea, intenta nuevamente'
+    end
+  end
+
+  def show
+  end
+
   
   
+  
+  
+
+  private
+
+  def set_todo
+    @todo = Todo.find(params[:id])
+  end
+  
+
+  def todo_params
+    params.require(:todo).permit(:description, :completed)
+  end
 end
+
